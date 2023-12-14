39c3e97b1863614dfe1f92d4250076757afcb7fd
Dakouri M. Kobri <maurille.kobri@gmail.com>
2023-12-14 17:05:08 +0100

    Initial commit for login-modal project

---
+++ login-modal/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export const content = ['./*.html'];
+export const theme = {
+  extend: {
+    fontFamily: {
+      sans: ['Mulish', 'sans-serif'],
+      mono: ['Rokkitt', 'monospace'],
+    },
+  },
+};
+export const plugins = [];
